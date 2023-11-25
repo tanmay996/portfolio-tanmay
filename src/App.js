353de66876bfce41c50e8216d6cfacdd9df7dca8
tanmay996 <110028920+tanmay996@users.noreply.github.com>
2023-11-25 16:07:11 +0530

    test16

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/js/dist/offcanvas';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Header from './components/Header';
@@ -14,20 +14,44 @@ import Contanct from './components/Contanct';
 import Footer from './components/Footer';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    AOS.init();
+    // Simulate a delay for data fetching or content loading
+    const timeout = setTimeout(() => {
+      setLoading(false);
+      // Initialize AOS when the content is loaded
+      AOS.init({
+        // AOS configuration options
+        // duration: 800,
+        // Other options...
+      });
+    }, 3000); // Simulated 2-second delay for demonstration
+
+    return () => clearTimeout(timeout);
   }, []);
 
   return (
     <div className='home bg-black' >
-     <Header /> 
-     <About /> 
-     <Resume   /> 
-      <Service />
-      <Skill />
-      <Project />
-      <Contanct  />
-      <Footer/>
+
+      {loading ? (
+        // Loader component while content is loading
+        <div id="loader">
+          <div class="loader-gif"></div>
+        </div>
+      ) : (
+        // Content with AOS animations
+        <div>
+          <Header />
+          <About />
+          <Resume />
+          <Service />
+          <Skill />
+          <Project />
+          <Contanct />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
